b45844fdc83e24aebf3c5d90f35e126a64849677
dazmag <iscope09@hotmail.com>
2020-09-08 02:34:21 -0400

    bonus

--- dom_practice.js
+++ dom_practice.js
@@ -16,12 +16,9 @@ document.querySelector('h2').innerHTML = "Welcome to my Boston!!!"
 
 const kish = document.getElementsByClassName("fun")
 console.log([kish])
-
-let text = document.querySelector(".Kay")
-text.style.color = "green"
-
-
-
+//Commented it off to do the bonus question
+//let text = document.querySelector(".Kay")
+//text.style.color = "green
 document.querySelector('.Pub').innerHTML = "Jamboree sandwich at the Jambo-ree"
 
 const magz = document.createElement("img")
@@ -29,3 +26,12 @@ const magz = document.createElement("img")
 magz.setAttribute("src", "https://whdh.com/wp-content/uploads/sites/3/2017/12/boston_skyline_over_the_charles_river.jpg?quality=60&strip=color&w=800")
 const fun = document.querySelector(".fun")
 fun.appendChild(magz)
+
+//Bonus
+let text = document.querySelector("ol")
+text.style.color = "green"
+/*
+const final = document.querySelector(".Tea")
+console.log(final)
+final.remove()
+*/
