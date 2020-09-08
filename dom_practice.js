0c40e72273ba464d881a5a180bd8b1124aeedf82
dazmag <iscope09@hotmail.com>
2020-09-08 01:51:52 -0400

    console logging the variables

--- dom_practice.js
+++ dom_practice.js
@@ -1,1 +1,21 @@
-console.log("Hello world")
+const world = console.log("Hello Boston")
+const kik = document.querySelector("footer")
+console.log(kik)
+
+const cat= document.querySelectorAll("ol")
+ console.log(cat)
+
+
+const zeus = document.querySelector("#Boston")
+console.log(zeus)
+
+const mim = document.querySelector("h2")
+console.log(mim)
+
+document.querySelector('h2').innerHTML = "Welcome to my Boston!!!"
+
+const kish = document.getElementsByClassName("fun")
+console.log([kish])
+
+let text = document.querySelector("li")
+text.style.color = "green"
